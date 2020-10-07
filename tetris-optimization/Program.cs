82c52f9e7d076a0ad090d14f3c6d6a009b0b3eba
skdw <kamil.gorzynski8@gmail.com>
2020-10-07 15:37:48 +0200

    Add .NET Core console app template

---
+++ tetris-optimization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tetris_optimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
