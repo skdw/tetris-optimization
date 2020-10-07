c41eccdcd1e1badb7cad1c0f911b2006e9311504
skdw <kamil.gorzynski8@gmail.com>
2020-10-07 17:26:36 +0200

    Add board with tetris blocks

---
+++ tetris-optimization/Blocks.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tetris_optimization.blocks
+{
+    public static class Blocks
+    {
+        // Tetris blocks
+        // https://www.quora.com/What-are-the-different-blocks-in-Tetris-called-Is-there-a-specific-name-for-each-block
+
+        // 1111
+        static readonly bool[,] I = {
+            { true, true, true, true }
+        };
+
+        // 100
+        // 111
+        static readonly bool[,] J = {
+            { true, false, false },
+            { true, true, true }
+        };
+
+        // 110
+        // 011
+        static readonly bool[,] Z = {
+            { true, true, false },
+            { false, true, true }
+        };
+
+        // 010
+        // 111
+        static readonly bool[,] T = {
+            { false, true, false },
+            { true, true, true }
+        };
+
+        // 011
+        // 110
+        static readonly bool[,] S = {
+            { false, true, true },
+            { true, true, false }
+        };
+
+        // 11
+        // 11
+        static readonly bool[,] O = {
+            { true, true },
+            { true, true }
+        };
+
+        // 001
+        // 111
+        static readonly bool[,] L = {
+            { false, false, true },
+            { true, true, true }
+        };
+
+        static readonly List<bool[,]> blocks = new List<bool[,]> { I, J, Z, T, S, O, L };
+
+        static readonly Random random = new Random();
+
+        static int GetBlockId() => random.Next(blocks.Count);
+
+        public static bool[,] GetBlock() => blocks[GetBlockId()];
+    }
+}
--- tetris-optimization/Program.cs
+++ tetris-optimization/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using tetris_optimization.blocks;
 
 namespace tetris_optimization
 {
     class Program
     {
+        class Board
+        {
+            public Board(int x, int y)
+            {
+                B = new bool[x, y];
+            }
+
+            readonly bool[,] B;
+
+            public void Print()
+            {
+                for (int i = 0; i < B.GetLength(0); ++i)
+                {
+                    Console.Write("|");
+                    for (int j = 0; j < B.GetLength(1); ++j)
+                        if (B[i, j])
+                            Console.Write("#");
+                        else
+                            Console.Write(" ");
+                    Console.Write("|\n");
+                }
+            }
+
+            public void Add(int x, int y, bool[,] block)
+            {
+                for (int i = 0; i < block.GetLength(0); ++i)
+                    for (int j = 0; j < block.GetLength(1); ++j)
+                        if ((x + i >= B.GetLength(0)) || (y + j >= B.GetLength(1)))
+                            Console.Error.WriteLine("Out of the board");
+                        else if(B[x + i, y + j] == true && block[i,j] == true)
+                            Console.Error.WriteLine("Trying to override the block");
+                        else
+                            B[x + i, y + j] = block[i, j];
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Board board = new Board(15, 20);
+            board.Add(0, 0, Blocks.GetBlock());
+            board.Add(5, 3, Blocks.GetBlock());
+            board.Add(10, 2, Blocks.GetBlock());
+            board.Add(8, 7, Blocks.GetBlock());
+            board.Add(5, 13, Blocks.GetBlock());
+            board.Add(10, 16, Blocks.GetBlock());
+            board.Print();
         }
     }
 }
