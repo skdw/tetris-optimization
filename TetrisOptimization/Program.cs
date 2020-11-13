adc59e702a99181146a0d30d1e647ebfbedc5e53
krzeminskamaja <mayaksheminska@gmail.com>
2020-11-13 15:32:43 +0100

    heuristic cutting maja

--- TetrisOptimization/Program.cs
+++ TetrisOptimization/Program.cs
@@ -21,19 +21,76 @@ namespace TetrisOptimization
             board.TryToAdd(0, 2, FiveBlocks.GetBlock(4));
             board.TryToAdd(3, 0, FiveBlocks.GetBlock(2));
             board.PrintBoard();
-            CuttingRectangle.Cutting(board, (6, 5), (0, 6), (0, 5));
+            //CuttingRectangle.Cutting(board, (6, 5), (0, 6), (0, 5));
 
-            List<(int, Block)> blocks = new List<(int, Block)>() { (2, FiveBlocks.GetBlock(1)), (1, FiveBlocks.GetBlock(3)), (1, FiveBlocks.GetBlock(6)) };
-            int blockSize = 5;
-            HeuristicSquare heuristicSquare = new HeuristicSquare(blocks, blockSize, 600, 0.4, 1);
-            heuristicSquare.Solve().Print(true);
-            Console.WriteLine("Minimal square calculated by heuristic algorithm: " + heuristicSquare.minimalAchivedSize);
+            //List<(int, Block)> blocks = new List<(int, Block)>() { (2, FiveBlocks.GetBlock(1)), (1, FiveBlocks.GetBlock(3)), (1, FiveBlocks.GetBlock(6)) };
+            //int blockSize = 5;
+            //HeuristicSquare heuristicSquare = new HeuristicSquare(blocks, blockSize, 600, 0.4, 1);
+            //heuristicSquare.Solve().Print(true);
+            //Console.WriteLine("Minimal square calculated by heuristic algorithm: " + heuristicSquare.minimalAchivedSize);
 
-            PreciseSquareSolver squareSolver = new PreciseSquareSolver(blocks, blockSize);
-            squareSolver.Solve().Print(true);
+            //PreciseSquareSolver squareSolver = new PreciseSquareSolver(blocks, blockSize);
+            //squareSolver.Solve().Print(true);
 
-            
+            //ciecia
+            //var test = FiveBlocks.GetBlock(5);
+            //Console.WriteLine(CuttingRectangle.HowManyUnitCuts(test));
+            //Console.WriteLine("Exxact Fit test");
+            //var block1 = FiveBlocks.GetBlock(5);
+            //var mtrx1 = new ConsoleColor?[2, 3]
+            //{
+            //    {null,null,null },
+            //    { null,null,ConsoleColor.Red}
+            //};
+            //var gap1 = new Gap(mtrx1, (2, 3), (0,0));
+            //var block2 = FiveBlocks.GetBlock(1);
+            //var mtrx2 = new ConsoleColor?[3, 3]
+            //{
+            //    { ConsoleColor.Red, null, null},
+            //{ null, null, ConsoleColor.Red},
+            //{ ConsoleColor.Red, null, ConsoleColor.Red}
+            //};
+            //var gap2 = new Gap(mtrx2, (3, 3), (2, 3));
+            //var block3 = FiveBlocks.GetBlock(6);//od 5
+            //var gap3 = new Gap(mtrx2, (3, 3), (5, 0));
+            //var boardTest3 = new Board(10, 10);
+            //var ef = CuttingRectangle.ExactFit(new List<Gap> { gap1, gap2, gap3 }, new List<Block> { block1, block2, block3 }, boardTest3);
+            //Console.WriteLine(ef.Item1.Count);
+            //Console.WriteLine(ef.Item2.Count);
+            //boardTest3.PrintBoard();
+            //var uc = CuttingRectangle.UnitCut(ef, boardTest3, 0);
+            //Console.WriteLine(uc);
+            //boardTest3.PrintBoard();
+            //Console.WriteLine("Check area up test");
+            //var boardTest4 = new Board(10, 10);
+            //boardTest4.TryToAdd(7, 5, new Block(new bool[2, 3] { { true, true, true }, { true, true, false } }, (3, 2)));
+            ////boardTest4.PrintBoard();
+            //Block test4 = CuttingRectangle.CheckAreaRight(boardTest4, 6, 8);
+            //Console.WriteLine();
+            //Console.WriteLine("GetCutBlock test");
+            //var boardTest5 = new Board(5, 5);
+            //boardTest5.TryToAdd(0, 0, FiveBlocks.GetBlock(6));
+            //boardTest5.TryToAdd(2, 0, FiveBlocks.GetBlock(9));
+            ////boardTest5.PrintBoard();
+            //var cb = CuttingRectangle.GetCutBlock(boardTest5);
+            //var board6 = new Board(10, 10);
+            //board6.TryToAdd(0, 0, cb[0]);
+            //board6.TryToAdd(5, 0, cb[1]);
+            //board6.PrintBoard();
+            //Console.WriteLine(cb.Count);
 
+            //Console.WriteLine(CuttingRectangle.DoesBlockFit(block1, gap1));
+            Console.WriteLine("Length cut test");
+            var board7 = new Board(6, 6);
+            board7.TryToAdd(0, 2, FiveBlocks.GetBlock(6));
+            board7.PrintBoard();
+            Console.WriteLine();
+            var gaps = new List<Gap>();
+            var g = new Gap(new ConsoleColor?[2, 2] { { null, null }, { null, null } }, (2, 2), (4, 4));
+            gaps.Add(g);
+            var lc = CuttingRectangle.LengthCut(board7, (2, 5, 1, 4), (0, 5), (0, 5), gaps);
+            lc.Item2.PrintBoard();
+            Console.WriteLine(lc.Item1);
         }
     }
 }
