d6402ef220b6001466d58cb17cb94abe22d8f236
Anna Buchman <01130560@pw.edu.pl>
2020-11-16 18:51:00 +0100

    two unit tests

---
+++ TetrisOptimization.Tests/TetrisOptimization_FindingGaps.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace TetrisOptimization.UnitTests
+{
+    public class TestFingingGaps
+    {
+        Board board;
+        [SetUp]
+        public void Setup()
+        {
+            board = new Board(6, 5);
+            board.TryToAdd(0, 1, new FiveBlock(2));
+            board.TryToAdd(2, 0, new FiveBlock(4));
+            board.TryToAdd(0, 3, new FiveBlock(2));
+        }
+        [Test]
+        public void TestFindingGaps()
+        {            
+            FindingGaps findingGaps= new FindingGaps(board);
+            List<Gap> gaps = findingGaps.FindGaps((0, 4, 0, 4));
+
+            List<(int, int)> fields = new List<(int, int)>() { (0, 0), (1, 0), (1, 1), (2, 1), (2, 0) };
+            Gap g = new Gap((3, 2), (0, 0), fields);
+            
+            Assert.AreEqual(gaps[0].fields, fields);
+        }
+        [Test]
+        public void TestPrepareMatrix()
+        {
+            List<(int, int)> fields = new List<(int, int)>() { (0, 0), (1, 0), (1, 1), (2, 1), (2, 0) };
+            int[,] matrix = TetrisOptimization.FindingGaps.prepareMatrix((3,2), (0,0), fields);
+            int[,] tmpmatrix = new int[3, 2];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 2; j++)
+                    if (i != 0 || j != 1)
+                        tmpmatrix[i, j]++;
+            Assert.AreEqual(matrix, tmpmatrix);
+        }
+    }
+}
